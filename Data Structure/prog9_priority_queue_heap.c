2855289f4d3fdc6429437083679132d2ec583d48
Praneeth <18praneeth@gmail.com>
2020-12-30 14:02:39 +0530

    Hasing

---
+++ Data Structure/prog9_priority_queue_heap.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+// Bottom Up method
+void heapify(int a[10], int n){
+    int i, k, j, v, flag;
+    for(i=n/2;i>=1;i--){
+        k = i;
+        v = a[k];
+        flag = 0;
+        while(!flag && 2*k <= n){
+            j = 2*k;
+            if (j < n){
+                if(a[j] < a[j+1])
+                    j = j+1;
+            }
+            if(v >= a[j]){
+                flag = 1;
+            }
+            else{
+                a[k] = a[j];
+                k = j;
+            }
+        }
+        a[k] = v;
+    }
+}
+
+
+int main(){
+    int i, n, ch, a[10] ;
+    for(;;){
+        printf("1. Create heap\n");
+        printf("2. Delete\n");
+        printf("3. Exit\n");
+        printf("Read choice:");
+        scanf("%d", &n);
+        switch(ch)
+        {
+            case 1: printf("\nRead no. of elements");
+                    scanf("%d", &n);
+                    printf("\nRead elements\n");
+                    for (i = 1; i < n; i++)
+                    {
+                        scanf("%d", &a[i]);
+                    }
+                    heapify(a, n);
+                    printf("\nElements after heapify");
+                    for (i = 1; i < n; i++)
+                    {
+                        printf("%d\t", a[i]);
+                    }
+                    break;
+            case 2: if(n >= 1){
+                    printf("\nElement del is %d", a[1]);
+                    a[1] = a[n];
+                    n = n - 1;
+                    heapify(a, n);
+                    printf("Elements after deletion\n");
+                    for (i = 1; i < n; i++)
+                    {
+                        printf("%d\t", a[i]);
+                    }
+                    }
+                    else{
+                    printf("\nNo elements for deletion");
+                    break;
+                    }
+            default: exit(0);
+
+        }
+    }
+    return 0;
+}
